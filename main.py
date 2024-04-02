397f265c7db2053c223dd3d9c3c256109dccba62
Francisco_GP <91037607+PACKYTO23@users.noreply.github.com>
2024-04-02 16:15:02 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,81 @@
+import requests
+import datetime as dt
+
+STOCK_NAME = "TSLA"
+COMPANY_NAME = "Tesla Inc"
+API_KEY = "TDSJUCJ8MO3ZZDJK"
+
+STOCK_ENDPOINT = "https://www.alphavantage.co/query"
+NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+
+parameters = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": STOCK_NAME,
+    "apikey": API_KEY,
+}
+
+# STEP 1: Use https://www.alphavantage.co/documentation/#daily
+# When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+
+#TODO 1. - Get yesterday's closing stock price.
+# Hint: You can perform list comprehensions on Python dictionaries.
+# e.g. [new_value for (key, value) in dictionary.items()]
+response = requests.get(STOCK_ENDPOINT, params=parameters)
+
+response.raise_for_status()
+
+data = response.json()["Time Series (Daily)"]
+data_list = [value for (key, value) in data.items()]
+
+#  today = dt.date.today()
+#  yesterday = str(today - dt.timedelta(days=1))
+#  before_yesterday = str(today - dt.timedelta(days=2))
+yesterday_data = data_list[0]
+yesterday_stock_close = float(yesterday_data["4. close"])
+
+#TODO 2. - Get the day before yesterday's closing stock price
+before_yesterday_data = data_list[1]
+before_yesterday_stock_close = float(before_yesterday_data["4. close"])
+
+#TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20.
+# Hint: https://www.w3schools.com/python/ref_func_abs.asp
+stocks_difference = yesterday_stock_close - before_yesterday_stock_close
+abs_stocks_difference = abs(stocks_difference)
+
+#TODO 4. - Work out the percentage difference in price
+# between closing price yesterday and closing price the day before yesterday.
+percentage_difference = (abs_stocks_difference / yesterday_stock_close) * 100
+#  x_percentage_difference = ((yesterday_stock_close * 100) / before_yesterday_stock_close) - 100
+
+#TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
+if percentage_difference > 5:
+    print("Get News")
+
+# STEP 2: https://newsapi.org/
+# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
+
+#TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
+
+#TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
+
+
+    ## STEP 3: Use twilio.com/docs/sms/quickstart/python
+    #to send a separate message with each article's title and description to your phone number. 
+
+#TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
+
+#TODO 9. - Send each article as a separate message via Twilio. 
+
+
+
+#Optional TODO: Format the message like this: 
+"""
+TSLA: 🔺2%
+Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
+Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
+or
+"TSLA: 🔻5%
+Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
+Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
+"""
+
